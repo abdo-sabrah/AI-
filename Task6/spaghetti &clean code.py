7a1d9470f9c53c466b602b925d0a8fdfccf1b89c
abdo-sabrah <110054747+abdo-sabrah@users.noreply.github.com>
2022-08-28 02:19:08 +0200

    Add files via upload

---
+++ Task6/spaghetti &clean code.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Spaspaghetti code
+print ("enter 2 numbers")
+x=int(input())
+y=int(input())
+z=x+y 
+print ("the result is ",z)
+
+
+# In[7]:
+
+
+#clean code 
+#function to add 2 numbers 
+def addition ():
+    print ("please enter 2 numbers")
+    number1 = int(input())
+    number2 = int(input())
+    #the adition method
+    result = number1 + number2 
+    print("the result is ",result)
+#calling addition function ;
+addition()
+
+
+# In[ ]:
+
+
+
+
